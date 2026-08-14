6e7e6cb12688143ee02d2af0641aca449c492dbf
Nehuen Lian <nehuenn.lian@gmail.com>
2026-08-14 04:15:25 -0300

    invert flag bool values

--- main.c
+++ main.c
@@ -10,19 +10,19 @@ struct Chunk {
         void *usable_size;
 };
 
-int is_first_allocation = 0; // 1 = false | 0 = true
+int is_first_allocation = 1; // 1 = true | 0 = false
 static struct Chunk *first_allocated_chunk;
 
 void* my_malloc(int bytes)
 {
-        if (is_first_allocation == 0) {
+        if (is_first_allocation == 1) {
                 first_allocated_chunk = sbrk(sizeof(struct Chunk) + bytes);
                 first_allocated_chunk->size = bytes;
                 first_allocated_chunk->free = 'n';
                 first_allocated_chunk->next_chunk = first_allocated_chunk;
                 first_allocated_chunk->prev_chunk = first_allocated_chunk;
 
-                is_first_allocation = 1;
+                is_first_allocation = 0;
 
                 return &first_allocated_chunk->usable_size;
         } else {
