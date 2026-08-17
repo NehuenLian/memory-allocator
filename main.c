ad00771c64bf12b72b05fa9a194d80adf4c809ea
Nehuen Lian <nehuenn.lian@gmail.com>
2026-08-17 16:12:04 -0300

    add comment for x_free function

--- main.c
+++ main.c
@@ -64,6 +64,13 @@ void* x_malloc(int bytes)
 
 void x_free(void *data_ptr)
 {
+/*
+        Searchs for an already free chunk with the requested size
+        or larger and returns a pointer to usable_size sector of
+        the chunk found or NULL if doesn't find any.
+        Returning NULL makes easier to verify in the caller if a
+        free chunk has been found or not.
+*/
         if (data_ptr == NULL) {
                 return;
         }
