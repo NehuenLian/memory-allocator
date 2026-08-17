b6a34e296f11e2ee054788f9d48417e67262fd05
Nehuen Lian <nehuenn.lian@gmail.com>
2026-08-17 16:38:18 -0300

    rename 'bytes' variable in all functions to 'bytes_requested' for clarity

--- main.c
+++ main.c
@@ -16,31 +16,31 @@ struct Chunk {
 int is_first_allocation = TRUE;
 static struct Chunk *first_allocated_chunk;
 
-int round_up_bytes(int bytes)
+int round_up_bytes(int bytes_requested)
 {
-        if (bytes < 16) {
+        if (bytes_requested < 16) {
                 return 16;
         }
 
         int r = 16;
         int result;
-        for (int i = 1; result < bytes; i++) {
+        for (int i = 1; result < bytes_requested; i++) {
                 result = r*i;
         }
-        int diff = result - bytes;
+        int diff = result - bytes_requested;
 
-        return bytes + diff;
+        return bytes_requested + diff;
 }
 
-void* x_malloc(int bytes)
+void* x_malloc(int bytes_requested)
 {
-        if (bytes % 16 != 0) {
-                bytes = round_up_bytes(bytes);
+        if (bytes_requested % 16 != 0) {
+                bytes_requested = round_up_bytes(bytes_requested);
         }
 
         if (is_first_allocation == TRUE) {
-                first_allocated_chunk = sbrk(sizeof(struct Chunk) + bytes);
-                first_allocated_chunk->size = bytes;
+                first_allocated_chunk = sbrk(sizeof(struct Chunk) + bytes_requested);
+                first_allocated_chunk->size = bytes_requested;
                 first_allocated_chunk->free = FALSE;
                 first_allocated_chunk->next_chunk = first_allocated_chunk;
                 first_allocated_chunk->prev_chunk = first_allocated_chunk;
@@ -49,8 +49,8 @@ void* x_malloc(int bytes)
 
                 return &first_allocated_chunk->usable_size;
         } else {
-                struct Chunk *chunk = sbrk(sizeof(struct Chunk) + bytes);
-                chunk->size = bytes;
+                struct Chunk *chunk = sbrk(sizeof(struct Chunk) + bytes_requested);
+                chunk->size = bytes_requested;
                 chunk->free = FALSE;
 
                 chunk->prev_chunk = first_allocated_chunk->prev_chunk;
@@ -79,11 +79,11 @@ void x_free(void *data_ptr)
         *pptr = TRUE;
 }
 
-void* search_free_chunk(int bytes)
+void* search_free_chunk(int bytes_requested)
 {
         struct Chunk *chunk = first_allocated_chunk;
         do {
-                if (chunk->size >= bytes) {
+                if (chunk->size >= bytes_requested) {
                         return &chunk->usable_size;
                 }
                 chunk = chunk->next_chunk;
